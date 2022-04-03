1b5b65ff3eba6c5e1f2a708fca68b6039f56c3a7
Pedro Serejo <serejoph@me.com>
2022-04-02 22:06:19 -0300

    Refatorando e criando listas

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,14 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,54 +21,25 @@ public class Program {
 
 	public static void main(String[] args) throws IOException {
 
-		InputStream file = new FileInputStream("imdbKey.properties");
-		Properties props = new Properties();
-		props.load(file);
+		List<String> movies = getMovies();
+		List<String> ids = getField(movies, "id");
+		List<String> ranks = getField(movies, "rank");
+		List<String> titles = getField(movies, "title");
+		List<String> fullTitles = getField(movies, "fullTitle");
+		List<String> year = getField(movies, "year");
+		List<String> images = getField(movies, "image");
+		List<String> crew = getField(movies, "crew");
+		List<String> imDbRating = getField(movies, "imDbRating");
+		List<String> imDbRatingCount = getField(movies, "imDbRatingCount");
+		
+		
+		
+		
+	
+		
+		
+		
 
-		URL url = new URL("https://imdb-api.com/pt-br/API/Top250Movies/" + props.getProperty("api_key"));
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("GET");
-		System.out.println(con.getResponseCode());
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-		String line = br.readLine();
-
-		Pattern p = Pattern.compile("\\[\\{.*\\}\\]");
-		Matcher m = p.matcher(line);
-		m.find();
-		String s = m.group();
-
-		System.out.println(s);
-
-		p = Pattern.compile("[{](.*?)[}]");
-		m = p.matcher(s);
-
-		List<String> movies = new ArrayList<>();
-
-		while (m.find()) {
-			movies.add(m.group(1).replace('"', Character.MIN_VALUE));
-		}
-		List<String> ids = new ArrayList<>();
-		movies.forEach(x ->{
-			String[] fields = x.split(",");
-			String[] id = fields[0].split(":");
-			ids.add(id[1]);
-		});
-		
-		ids.forEach(System.out::println);
-
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 //		JSONObject jObj = new JSONObject(line);
 //		JSONArray jArr = jObj.getJSONArray("items");
 //		
@@ -77,5 +52,44 @@ public class Program {
 //		
 //		System.out.println(mov);
 
+	}
+
+	private static List<String> getMovies()
+			throws FileNotFoundException, IOException, MalformedURLException, ProtocolException {
+		InputStream file = new FileInputStream("imdbKey.properties");
+		Properties props = new Properties();
+		props.load(file);
+
+		URL url = new URL("https://imdb-api.com/API/Top250Movies/" + props.getProperty("api_key"));
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String line = br.readLine();
+
+		Pattern p = Pattern.compile("[{]([^{]*)[}]");
+		Matcher m = p.matcher(line);
+	
+		List<String> movies = new ArrayList<>();
+
+		while (m.find()) {
+			movies.add(m.group(1));
+		}
+		return movies;
+	}
+
+	private static List<String> getField(List<String> movies, String field) throws IOException {
+
+		Pattern p = Pattern.compile(field + "\":\"(.*?)\"");
+		FileWriter fw = new FileWriter("output.txt");
+		List<String> list = new ArrayList<>();
+		for (String s : movies) {
+						
+			Matcher m = p.matcher(s);
+			if (m.find()) {
+				list.add(m.group(1));
+			}
+		}
+
+		return list;
 	}
 }
